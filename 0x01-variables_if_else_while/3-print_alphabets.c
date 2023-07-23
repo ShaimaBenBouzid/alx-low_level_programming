e08fc3113d77f398153d16f2d30ee111e3445303
ShaimaBenBouzid <mhamdishaimaa@gmail.com>
2023-07-23 05:06:15 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,19 @@ int main(void)
 {
 	char c;
 
+	char i;
+
 	c = 'a';
+	d = 'A';
 	while
 		(c <= 'z') {
 			putchar(c);
 			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(i);
+			i++;
 		}
 	putchar('\n');
 	return (0);
