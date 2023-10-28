7ad98633e3156e97ac2878e7fcb50b57a3aee5e5
root <mhamdishaimaa@gmail.com>
2023-10-28 13:22:25 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@
  *
  * Return: number of bits to flip to convert numbers
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int n)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xorval = n ^ m;
 	unsigned int count = 0;
